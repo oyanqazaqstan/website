dc0e799f10d2c50ddeba4884cd7d2c9a66709b56
Zharzhan Kulmyrza <zharzhan@gmail.com>
2019-08-18 01:44:35 +0200

    Chunk of small design improvements

--- src/js/components/theme-toggle.js
+++ src/js/components/theme-toggle.js
@@ -59,8 +59,8 @@ class ThemeToggle extends HTMLElement {
 
   setButtonLabelAndStatus(currentSetting) {
     this.modeToggleButton.innerText = `${
-      currentSetting === 'dark' ? 'Light' : 'Dark'
-    } theme`;
+      currentSetting === 'dark' ? 'Светлая' : 'Тёмная'
+    } тема`;
     this.modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
   }
 
@@ -69,7 +69,7 @@ class ThemeToggle extends HTMLElement {
       <div class="[ theme-toggle ] [ md:ta-right gap-top-500 ]">
         <div role="status" class="[ visually-hidden ][ js-mode-status ]"></div>
         <button class="[ button ] [ font-base text-base weight-bold ] [ js-mode-toggle ]">
-          Dark theme
+          🌑
         </button>
       </div>
     `;
